9740af12cfd835276b5c810f41f56817545af872
ericgrossman5455 <ericg5455@gmail.com>
2019-06-20 23:26:26 -0700

    Added credit to Ed

--- nav.js
+++ nav.js
@@ -1,9 +1,22 @@
+//Thank you to Ed at YouTube Channel: Dev Ed
+
 const navSlide = () => {
     const drawer = document.querySelector('.drawer');
     const nav = document.querySelector('nav');
     const shadow = document.querySelector('.nav_shadow');
+    const back = document.querySelector('.nav_back_arrow');
     
     drawer.addEventListener('click', () => {
+        nav.classList.toggle('nav_active');
+        shadow.classList.toggle('nav_shadow_active');
+    });
+    
+    shadow.addEventListener('click', () => {
+        nav.classList.toggle('nav_active');
+        shadow.classList.toggle('nav_shadow_active');
+    });
+    
+    back.addEventListener('click', () => {
         nav.classList.toggle('nav_active');
         shadow.classList.toggle('nav_shadow_active');
     });
@@ -11,19 +24,3 @@ const navSlide = () => {
 }
 
 navSlide();
-
-//const navSlide = () => {
-//    const drawer = document.querySelector('.drawer');
-//    const nav = document.querySelector('nav');
-//    const shadow = document.querySelector('.nav_shadow');
-//    
-//    drawer.addEventListener('click',()=>{
-//        nav.classList.toggle('nav_active');
-//    });
-//    
-//    drawer.addEventListener('click',()=>{
-//        .nav_shadow.classList.toggle('.nav_shadow_active');
-//    });
-//}
-//
-//navSlide();
