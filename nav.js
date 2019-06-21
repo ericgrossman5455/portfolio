5ab37918e94b418b28192b3008038784e68510e3
ericgrossman5455 <ericg5455@gmail.com>
2019-06-20 22:34:26 -0700

    nav shadow working (no exit yet)

--- nav.js
+++ nav.js
@@ -1,10 +1,13 @@
 const navSlide = () => {
     const drawer = document.querySelector('.drawer');
     const nav = document.querySelector('nav');
+    const shadow = document.querySelector('.nav_shadow');
     
     drawer.addEventListener('click', () => {
         nav.classList.toggle('nav_active');
+        shadow.classList.toggle('nav_shadow_active');
     });
+
 }
 
 navSlide();
