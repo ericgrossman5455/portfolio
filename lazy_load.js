6b171034a9de9de264d10a85874f82d04cb6cba9
ericgrossman5455 <ericg5455@gmail.com>
2019-08-28 18:22:31 -0700

    Adjustment to lazy_load

--- lazy_load.js
+++ lazy_load.js
@@ -11,7 +11,7 @@ function preloadImage(img) {
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px 300px 0px"
+    rootMargin: "0px 0px 800px 0px"
 };
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
