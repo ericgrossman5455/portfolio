30dca02846752b5add13f1ab493ebb37d088274b
ericgrossman5455 <ericg5455@gmail.com>
2019-12-30 21:57:05 -0800

    Implemented smooth auto scrolling

---
+++ smooth_scroll.js
@@ -0,0 +1,12 @@
+// A big thank you to Tania Rascia!
+
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	}, 500,
+    'linear');
+});
