436d48717c23a574f801dc7b9150f1c652ea447d
ericgrossman5455 <ericg5455@gmail.com>
2020-04-04 15:59:02 -0700

    sm removal

--- scripts/not_available.js
+++ scripts/not_available.js
@@ -1,3 +1,3 @@
 function notAvailable() {
-    alert("Unfortunately Eric hasn't finished developing this feature. Try again later. PS Alex is cute.");
+    alert("Unfortunately Eric hasn't finished developing this feature. Try again later.");
   }
