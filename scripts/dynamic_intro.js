43ade16fe92e2f589256ecad065070a87c1e4d15
ericgrossman5455 <ericg5455@gmail.com>
2020-04-20 15:54:14 -0700

    first commit

---
+++ scripts/dynamic_intro.js
@@ -0,0 +1,19 @@
+var date = new Date();
+var hour = date.getHours();
+var intro = document.querySelector(".intro h1");
+
+if (hour < 3){ //earlier than 3AM
+    intro.innerHTML = 'hey night owl.';
+} else if (hour < 7){ //earlier than 7AM
+    intro.innerHTML = 'hey early bird.';
+} else if (hour < 10){ //earlier than 10AM
+    intro.innerHTML = 'good mornin\’.';
+} else if (hour < 13){ //earlier than 1PM
+    intro.innerHTML = 'hey there.';
+} else if (hour < 18){ //earlier than 6PM
+    intro.innerHTML = 'good<br>afternoon.';
+} else if (hour < 22){ //earlier than 10PM
+    intro.innerHTML = 'good evening.';
+} else if (hour <= 24){ //earlier or equal to 12AM
+    intro.innerHTML = 'hey night owl.';
+}
