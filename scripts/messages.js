0310cd109599d2d24f182569a805827eeb4c1824
ericgrossman5455 <ericg5455@gmail.com>
2020-04-20 15:54:28 -0700

    More scalable wording

---
+++ scripts/messages.js
@@ -0,0 +1,7 @@
+function noFeature() {
+  alert("Unfortunately Eric hasn't finished developing this feature. Please check back later.");
+}
+
+function noPage() {
+  alert("Unfortunately this page isn't ready for prime time. Please check back later.");
+}
--- scripts/not_available.js
+++
@@ -1,3 +0,0 @@
-function notAvailable() {
-    alert("Unfortunately Eric hasn't finished developing this feature. Try again later.");
-  }
