db7717fc13024213d3b624f751a2efd033d28028
Eric Grossman <eric_grossman@apple.com>
2020-10-19 22:34:25 -0700

    implemented dynamic intro resizing v2

--- scripts/dynamic-intro.js
+++ scripts/dynamic-intro.js
@@ -5,16 +5,20 @@ var headshot = document.querySelector(".hero-headshot");
 var signature = document.querySelector(".signature");
 
 if (hour < 3){ //earlier than 3AM
-    h1.innerHTML = 'hey night&nbsp;owl.';
-    headshot.style.height = '350px';
-    headshot.style.backgroundPosition = '-55px';
-    signature.style.bottom = '-40px';
+    h1.innerHTML = 'hey<br>night&nbsp;owl.';
+    headshot.style.height = '335px';
+    headshot.style.margin = '4px 30px 0 0';
+    headshot.style.backgroundPosition = '-40px';
+    signature.style.right = '-115px';
+    signature.style.bottom = '-50px';
 }
 else if (hour < 7){ //earlier than 7AM
     h1.innerHTML = 'hey early&nbsp;bird.';
-    headshot.style.height = '350px';
-    headshot.style.backgroundPosition = '-55px';
-    signature.style.bottom = '-40px';
+    headshot.style.height = '335px';
+    headshot.style.margin = '20px 30px 0 0';
+    headshot.style.backgroundPosition = '-40px';
+    signature.style.right = '-120px';
+    signature.style.bottom = '-45px';
 }
 else if (hour < 10){ //earlier than 10AM
     h1.innerHTML = 'good mornin\’.';
@@ -24,19 +28,25 @@ else if (hour < 13){ //earlier than 1PM
 }
 else if (hour < 18){ //earlier than 6PM
     h1.innerHTML = 'good<br>afternoon.';
-    headshot.style.height = '350px';
-    headshot.style.backgroundPosition = '-55px';
-    signature.style.bottom = '-40px';
+    headshot.style.height = '335px';
+    headshot.style.margin = '20px 30px 0 0';
+    headshot.style.backgroundPosition = '-40px';
+    signature.style.right = '-120px';
+    signature.style.bottom = '-45px';
 }
 else if (hour < 22){ //earlier than 10PM
     h1.innerHTML = 'good<br>evening.';
-    headshot.style.height = '350px';
-    headshot.style.backgroundPosition = '-55px';
-    signature.style.bottom = '-40px';
+    headshot.style.height = '335px';
+    headshot.style.margin = '20px 30px 0 0';
+    headshot.style.backgroundPosition = '-40px';
+    signature.style.right = '-120px';
+    signature.style.bottom = '-45px';
 }
 else if (hour <= 24){ //earlier or equal to 12AM
-    h1.innerHTML = 'hey night&nbsp;owl.';
-    headshot.style.height = '350px';
-    headshot.style.backgroundPosition = '-55px';
-    signature.style.bottom = '-40px';
+    h1.innerHTML = 'hey<br>night&nbsp;owl.';
+    headshot.style.height = '335px';
+    headshot.style.margin = '4px 30px 0 0';
+    headshot.style.backgroundPosition = '-40px';
+    signature.style.right = '-115px';
+    signature.style.bottom = '-50px';
 }
