b944034657ce19b6a242e6daf648036eb74847b1
Eric Grossman <ericg5455@gmail.com>
2021-05-15 17:53:14 -0700

    improved UX for missing about page

--- scripts/messages.js
+++ scripts/messages.js
@@ -2,6 +2,10 @@ function noFeature() {
   alert("Unfortunately Eric hasn't finished developing this feature. Please check back later.");
 }
 
+function noAboutPage() {
+  if(confirm("Eric hasn't finished developing this page.\nWould you like to view his LinkedIn instead?")) document.location = 'https://www.linkedin.com/in/eric-grossman-277a27bb/';
+}
+
 function noPage() {
   alert("Unfortunately this page isn't ready for prime time. Please check back later.");
 }
