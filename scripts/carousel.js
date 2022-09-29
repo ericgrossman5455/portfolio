97f12013f691b79dace425b0dc2d79e0f1425598
Eric Grossman <ericg5455@gmail.com>
2022-09-28 20:59:52 -0700

    brought back carousel elements

---
+++ scripts/carousel.js
@@ -0,0 +1,5 @@
+let card = document.querySelector(".js-card");
+let glow = document.querySelector(".project-tile-wrapper");
+
+
+card.addEventListener("click", function() { card.classList.add("active"), glow.classList.add("active")});
