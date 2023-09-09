6a018a283c6f0f0e05a62664ada026f95dbc7786
Eric Grossman <ericg5455@gmail.com>
2023-09-08 22:39:04 -0700

    chore: remove script

--- scripts/carousel.js
+++
@@ -1,5 +0,0 @@
-let card = document.querySelector(".js-card");
-let glow = document.querySelector(".project-tile-wrapper");
-
-
-card.addEventListener("click", function() { card.classList.add("active"), glow.classList.add("active")});
